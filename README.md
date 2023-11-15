267892aef0035a143b369c9082e6de4f0a976ba4
ThomasKWood <thomas.ke.wood@outlook.com>
2023-11-15 15:57:59 -0500

    small fixes. added comments

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # COMP3005-A4
+**Course**: COMP3005 - Database Management Systems\
+**Assignment**: Assignment 4\
+**Due Date**: 2023-11-23\
+**Author**: Thomas Wood
+
+## Demonstration Video
+[![Video_Title](https://img.youtube.com/vi/ID/hqdefault.jpg)](https://www.youtube.com/watch?v=ID "COMP3005-A4 Demo Video")\
+https://www.youtube.com/watch?v=ID
+
+## Getting Started
+
+To set up and use this program, follow these steps:
+
+1. **Instal Node.js**: This program is written using JavaScript and requires Node.js to run. To install Node.js, visit https://nodejs.org/en/download/ and download the appropriate installer for your operating system. Once downloaded, run the installer and follow the instructions to install Node.js.
+2. **Install PostgreSQL**: This program uses PostgreSQL as its database. To install PostgreSQL, visit https://www.postgresql.org/download/ and download the appropriate installer for your operating system. Once downloaded, run the installer and follow the instructions to install PostgreSQL.
+2. **Clone Repository**: Clone the repository to your local machine. If you are using Git, you can use the following command: `git clone https://github.com/ThomasKWood/COMP3005-A4.git`.
+3. **Create Database**: Create a new database in PostgreSQL called `comp3005-a4`. This can be done using the following command: `CREATE DATABASE comp3005-a4;`.
+4. **Run DDL**: In the [SQL/](SQL) directory, run the [DDL.psql](SQL/DDL.psql) file in your PostgreSQL database. This will create the student table and populate it with data.
+5. **Provide SQL Credentials**: In the [!SQLcreds.js](source/!SQLcreds.js) file, provide a username and password with read and write access to your PostgreSQL database in lines 5 and 6 respectively. Here you can also change the port number and host address if necessary.
+6. **Install Dependencies**: In the [source/](source) directory, run the following command: `npm install`. This will install all of the dependencies required to run the program.
+7. **Run Program**: In the [source/](source) directory, run the following command: `npm run start`. This will run the program.
+
+## Organization of the Submission
+
+The submission is organized into 2 folders:
+
+1. [source/](source): This folder contains all of the source code for the program.
+2. [SQL/](SQL): This folder contains the SQL code used to create the database and populate it with data.
--- source/main.js
+++ source/main.js
@@ -7,20 +7,33 @@
  */
 
 // Setup
-var pgp = require('pg-promise')();
-const sqlCreds = require('./!SQLcreds.js');
-const { as } = require('pg-promise');
-var db = pgp(sqlCreds);
+var pgp = require('pg-promise')();  // Library for connecting to postgresql databases
+const sqlCreds = require('./!SQLcreds.js'); // Contains the credentials for connecting to the database
+var db = pgp(sqlCreds); // Database object for connecting to the database
 
 // Functions
+/**
+ * Retrieves all students from the database.
+ * @returns {Promise<Array>} A promise that resolves to an array of student objects.
+ */
 async function getAllStudents() {
-    return await db.any('SELECT * FROM students');
+    return await db.any('SELECT * FROM students'); // any is used because the query returns multiple rows
 }
 
+/**
+ * Inserts a new student into the database.
+ * @async
+ * @function addStudent
+ * @param {string} first_name - The first name of the student.
+ * @param {string} last_name - The last name of the student.
+ * @param {string} email - The email address of the student.
+ * @param {string} enrollment_date - The enrollment date of the student in the format 'YYYY-MM-DD'.
+ * @returns {boolean} - Returns true if the student was inserted successfully, false otherwise.
+ */
 async function addStudent(first_name, last_name, email, enrollment_date) {
     let result = false;
-    let query = 'INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ($1, $2, $3, CAST($4 AS DATE))';
-    await db.none(query, [first_name, last_name, email, enrollment_date]).then(() => {
+    let query = 'INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ($1, $2, $3, CAST($4 AS DATE))'; // CAST is used to convert the string to a date
+    await db.none(query, [first_name, last_name, email, enrollment_date]).then(() => { // none is used because the query does not return any rows
         console.log('Student inserted successfully');
         result = true;
         return true;
@@ -32,10 +45,18 @@ async function addStudent(first_name, last_name, email, enrollment_date) {
     return result;
 }
 
+/**
+ * Updates the email of a student with the given student_id.
+ * @async
+ * @function updateStudentEmail
+ * @param {number} student_id - The ID of the student to update.
+ * @param {string} new_email - The new email to set for the student.
+ * @returns {Promise<boolean>} - A Promise that resolves to true if the email was updated successfully, false otherwise.
+ */
 async function updateStudentEmail(student_id, new_email) {
     let result = false;
-    let query = 'UPDATE students SET email = $1 WHERE student_id = $2';
-    await db.none(query, [new_email, student_id]).then(() => {
+    let query = 'UPDATE students SET email = $1 WHERE student_id = $2'; 
+    await db.none(query, [new_email, student_id]).then(() => { // none is used because the query does not return any rows
         console.log('Student email updated successfully');
         result = true;
         return true;
@@ -47,10 +68,17 @@ async function updateStudentEmail(student_id, new_email) {
     return result;
 }
 
+/**
+ * Deletes a student from the database.
+ * @async
+ * @function deleteStudent
+ * @param {number} student_id - The ID of the student to delete.
+ * @returns {Promise<boolean>} - A Promise that resolves to true if the student was deleted successfully, or false otherwise.
+ */
 async function deleteStudent(student_id) {
     let result = false;
     let query = 'DELETE FROM students WHERE student_id = $1';
-    await db.none(query, [student_id]).then(() => {
+    await db.many(query, [student_id]).then(() => { // many is used because it will trow an error if it deletes in less than 1 second
         console.log('Student deleted successfully');
         result = true;
         return true;
@@ -64,23 +92,36 @@ async function deleteStudent(student_id) {
 
 
 // Tester Functions
+/**
+ * Calls the getAllStudents function and logs the result to the console.
+ */
 async function testGetAllStudents() {
+    // get all students
     let result = await getAllStudents();
+    // print all students
     console.log("\nPrinting all students...");
     console.log(result);
 }
 
+/**
+ * Tests the addStudent function by adding a new student and verifying that it was added successfully.
+ * Also tests that adding a student with an existing email fails.
+ */
 async function testAddStudent() {
+    // print all students before we modify anything
     await testGetAllStudents();
 
+    // add a student
     console.log("\nAdding Thomas Wood...");
     let result = await addStudent('Thomas', 'Wood', 'thomaswood4@cmail.carleton.ca', '2020-09-05');
 
+    // print all students after we modify the db
     if (result) {
         console.log("\nThomas Wood added successfully\nPrinting Students...\n");
         await testGetAllStudents();
     }
 
+    // try to add a student with an existing email
     console.log("\nAdd a student with an existing email...");
     result = await addStudent('Bad', 'Student', 'john.doe@example.com', '2001-10-23');
     if (!result) {
@@ -88,17 +129,25 @@ async function testAddStudent() {
     }
 }
 
+/**
+ * Tests the updateStudentEmail function by updating John Doe's email to 'johndoe@cmail.carleton.ca'
+ * and then attempting to update it to an existing email. Prints all students before and after the updates.
+ */
 async function testUpdateStudentEmail() {
+    // print all students before we modify anything
     await testGetAllStudents();
 
+    // update a student's email
     console.log("\nUpdating Johm Doe's email...");
     let result = await updateStudentEmail(1, 'johndoe@cmail.carleton.ca');
 
+    // print all students after we modify the db
     if (result) {
         console.log("\nJohn Doe's email updated successfully\nPrinting Students...\n");
         await testGetAllStudents();
     }
 
+    // try to update a student's email to an existing email
     console.log("\Update a student with an existing email...");
     result = await updateStudentEmail(1, 'jane.smith@example.com');
     if (!result) {
@@ -106,12 +155,18 @@ async function testUpdateStudentEmail() {
     }
 }
 
+/**
+ * Deletes a student and tests if the deletion was successful by printing all students.
+ */
 async function testDeleteStudent() {
+    // print all students before we modify anything
     await testGetAllStudents();
 
+    // delete a student
     console.log("\nDeleting John Doe...");
     let result = await deleteStudent(1);
 
+    // print all students after we modify the db
     if (result) {
         console.log("\John Doe deleted successfully\nPrinting Students...\n");
         await testGetAllStudents();
@@ -119,15 +174,22 @@ async function testDeleteStudent() {
 }
 
 const readline = require('readline');
+/**
+ * Readline interface for user input/output.
+ * @type {readline.Interface}
+ */
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+/**
+ * Runs the main menu loop for the student db program.
+ */
 async function main() {
     let loop = true;
+    console.log("Main Menu:\n1. Get all students\n2. Add a student\n3. Update a student's email\n4. Delete a student\n5. Exit");
     while (loop) {
-        console.log("Main Menu:\n1. Get all students\n2. Add a student\n3. Update a student's email\n4. Delete a student\n5. Exit");
         await new Promise((resolve, reject) => {
             rl.question('Enter a number: ', (input) => {
                 switch (input) {
@@ -157,8 +219,4 @@ async function main() {
     rl.close();
 }
 
-
-
-if (true) {
-    main();
-}
+main();
